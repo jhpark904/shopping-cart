d3509b4b0cbfaecba37b2b19060d6c6c03744090
Jung Hwan Park <jhpark904@gmail.com>
2021-01-17 15:56:06 -0500

    create admin page

--- server.js
+++ server.js
@@ -80,5 +80,18 @@ app.post("/api/orders", async(req, res)=> {
     res.send(order)
 })
 
+// order api
+app.get("/api/orders", async(req, res) => {
+    // get all orders
+    const orders = await Order.find({})
+    res.send(orders)
+})
+
+// delete order
+app.delete("/api/orders/:id", async(req, res) => {
+    const order = await Order.findByIdAndDelete(req.params.id)
+    res.send(order)
+})
+
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log("server: http://localhost:5000"))
